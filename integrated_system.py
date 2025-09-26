54a8bbbbc37b81a7ea7334946e15e42e7f94062b
Hana Elfar <hanaelfaar2003@gmail.com>
2025-09-26 22:38:54 +0300

    Update integrated_system.py

--- integrated_system.py
+++ integrated_system.py
@@ -21,7 +21,7 @@ class IntegratedMediGuardSystem:
         self.rule_based_system = MediGuardRuleBasedSystem(language=language)
         
         # Load trained ECG classification model
-        self.ecg_model = tf.keras.models.load_model("model.h5")
+        self.ecg_model = tf.keras.models.load_model("ecg_model/ecg_classification_model.h5")
 
         self.ecg_scaler = MinMaxScaler()
         
